b579e7029da70160d4b49ed19a54b1cdc3a2ce2c
cocorobarestudio <fujiwara@cocorobare.com>
2020-12-24 15:22:04 +0900

    20201224

--- main.js
+++ main.js
@@ -73,13 +73,119 @@
 //     });
 // }
 
+// {
+//     document.querySelector('button').addEventListener('click', () =>{
+//       const item2 = document.createElement('li');
+//       item2.textContent = 'item2';
+
+//       const ul = document.querySelector('ul');
+//       ul.appendChild(item2);
+
+//     });
+// }
+// {
+//     document.querySelector('button').addEventListener('click', () => {
+//         const item0 = document.querySelectorAll('li')[0];
+//         const copy = item0.cloneNode(true);
+
+//         const ul = document.querySelector('ul');
+//         const item2 = document.querySelectorAll('li')[2];
+
+//         ul.insertBefore(copy, item2);
+
+//     });
+// }
+
+// {
+//     document.querySelector('button').addEventListener('click', () => {
+//         const item0 = document.querySelectorAll('li')[0];
+//         const copy = item0.cloneNode(true);
+
+//         const ul = document.querySelector('ul');
+//         const item2 = document.querySelectorAll('li')[2];
+
+//         ul.insertBefore(copy, item2);
+//     });
+// }
+
+// {
+//     document.querySelector('button').addEventListener('click',  () => {
+//         const item1 = document.querySelectorAll('li')[1];
+
+//         // item1.remove();
+//         document.querySelector('ul').removeChild(item1)
+//     }); 
+// }
+
+
+// {
+//     document.querySelector('button').addEventListener('click', () => {
+//         const li = document.createElement('li');
+//         const text = document.querySelector('input');
+//         li.textContent = text.value;
+//         document.querySelector('ul').appendChild(li);
+
+//         text.value = '';
+     
+//     });
+// }
+
+// {
+//     document.querySelector('button').addEventListener('click', () => {
+//         const li = document.createElement('li');
+//         const color = document.querySelector('select');
+//         li.textContent = `${color.value} - ${color.selectedIndex}`;
+//         document.querySelector('ul').appendChild(li);
+//     });
+// }
+
+// {
+//     document.querySelector('button').addEventListener('click', () => {
+//         const li = document.createElement('li');
+//         const color = document.querySelector('select');
+
+//         li.textContent = `${color.value} - ${color.selectedIndex}`;
+//         document.querySelector('ul').appendChild(li);
+//     });
+// }
+
+// {
+//     document.querySelector('button').addEventListener('click', () => {
+//         const colors = document.querySelectorAll('input');
+
+//         let selectedColor;
+        
+//         colors.forEach(color => {
+//             if (color.checked === true){
+//                 selectedColor = color.value;
+//             }
+//         });
+
+//         const li = document.createElement('li');
+//         li.textContent = selectedColor;
+//         document.querySelector('ul').appendChild(li);
+//     });
+// }
+
+// {
+//     document.querySelector('button').addEventListener('dblclick', () => {
+//         console.log('double clicked!');
+//     });
+// }
+// {
+// document.addEventListener('mousemove', e => {
+//     console.log(e.clientX, e.clientY);
+// });
+// }
+
+// {
+//     document.addEventListener('keydown' e => {
+//         console.log(e.key);
+//     });
+// }
+
 {
-    document.querySelector('button').addEventListener('click', () =>{
-      const item2 = document.createElement('li');
-      item2.textContent = 'item2';
+    const text = document.querySelector('textarea');
 
-      const ul = document.querySelector('ul');
-      ul.appendChild(item2);
-
-    });
+    text.addEventListener
 }
